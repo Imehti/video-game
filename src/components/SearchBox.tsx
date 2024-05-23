f8455c00529380d1f5b7eb5cdf65d0e6eae77bb3
Mahdi <khajavim497@gmail.com>
2024-05-23 19:33:35 +0430

    make search box responsive

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -3,11 +3,11 @@ function SearchBox() {
     <>
       <div className="relative">
         <input
-          className="rounded-2xl bg-searchIcon w-64 pl-10 font-mono"
+          className="rounded-2xl bg-searchIcon sm:w-64 w-44 sm:pl-10 pl-8 text-sm sm:text-md font-mono"
           type="text"
           placeholder="Discover New Games"
         />
-        <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray">
+        <span className="absolute sm:left-3 left-1 top-1/2 -translate-y-1/2 text-gray">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5 text-gray-400"
@@ -18,7 +18,7 @@ function SearchBox() {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth={2}
+            strokeWidth={1}
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           />
         </svg>
