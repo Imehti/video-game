3c3c27fb18ffdb7339bfd8c9efaf15b70e39a451
Mahdi <khajavim497@gmail.com>
2024-05-23 18:58:15 +0430

    use search box in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import SearchBox from "./SearchBox";
+
 function Navbar() {
   return (
     <>
@@ -6,8 +8,11 @@ function Navbar() {
         <img className="m-3 p-1 mr-1" src="src\assets\logo.png" alt="" />
         <h1 className="text-blue m-6 ml-1 font-mono text-2xl">Game</h1>
         <span className="border-r border-white h-8 -ml-2"></span>
-        <h1 className="text-white ml-3">Explore</h1>
+        <h1 className="text-white ml-4">Explore</h1>
         <h1 className="text-white ml-4">Blog</h1>
+        <div className="ml-8">
+        <SearchBox />
+        </div>
       </div>
       {/* end logo */}
     </>
