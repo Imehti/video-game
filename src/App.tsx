d58b8b75cd0e248fd2285e22d9d3f4b924d04dad
Mahdi <khajavim497@gmail.com>
2024-05-23 18:37:13 +0430

    add background image in home page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[url('./assets/home-bg.jpg')] h-screen bg-cover bg-center bg-no-repeat">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
