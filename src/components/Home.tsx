34951c5ef231d35d6e942bad8e81dda9a7dc013c
Mahdi <khajavim497@gmail.com>
2024-05-23 19:39:57 +0430

    change the text font

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,12 +6,12 @@ function Home() {
       <div className="bg-[url('./assets/home-bg.jpg')] h-screen bg-cover bg-center bg-no-repeat">
         <Navbar />
         <div className="flex items-center sm:mt-32 sm:ml-12 mt-11 ml-3">
-          <h1 className="text-white sm:text-3xl font-bold text-xl sm:tracking-widest">
+          <h1 className="text-white sm:text-4xl font-bold text-xl sm:tracking-widest">
             Discover Games You Will Love
           </h1>
         </div>
         <div className="flex flex-col sm:w-1/2 sm:ml-12 ml-3">
-          <p className="text-white font-mono mt-8">
+          <p className="text-white font-medium mt-8">
             Welcome, adventures! Are you ready to conquer epic quests build
             thriving empires, or solve mind-bending puzzles? We're your one-stop
             shop for discovering incredible games.
